b8aae56c4e50c872e80ec146a277701c191043fd
ZHO-ZHO-ZHO <140084057+ZHO-ZHO-ZHO@users.noreply.github.com>
2024-01-05 00:21:20 +0800

    Create AnyTextNodeTest.py 测试

---
+++ AnyTextNodeTest.py
@@ -0,0 +1,63 @@
+from modelscope.pipelines import pipeline
+
+class AnyTextNode_Zho:
+  
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "prompt": ("STRING", {"default": "A cake with colorful characters that reads "EVERYDAY"", "multiline": True}),
+                "mode": (["text-generation", "text-editing"],),  
+                "image_count": ("INT", {"default": 2, "min": 1, "max": 10}),
+                "ddim_steps": ("INT", {"default": 20, "min": 1, "max": 100}),
+                "show_debug": ("BOOLEAN", {"default": True}),
+                "seed": ("INT", {"default": 0, "min": 0, "max": 99999999}),
+                "draw_pos": ("IMAGE",),
+            },
+            "optional": {
+                "ori_image": ("IMAGE",),  
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    CATEGORY = "✒️AnyText"
+    FUNCTION = "run_anytext"
+
+    def run_anytext(self, mode, prompt, image_count, ddim_steps, show_debug, seed, draw_pos, ori_image):
+        
+        # Configure the pipeline for AnyText
+        pipe = pipeline('my-anytext-task', model='damo/cv_anytext_text_generation_editing', model_revision='v1.1.1', use_fp16=True, use_translator=False)
+        
+        self.params = {
+            "show_debug": show_debug,
+            "image_count": image_count,
+            "ddim_steps": ddim_steps,
+        }
+
+        input_data = {
+            "prompt": prompt,
+            "seed": seed,
+            "draw_pos": draw_pos
+        }
+
+        if mode == 'text-generation':
+            pass
+        else:
+            input_data["ori_image"] = ori_image
+
+        results, rtn_code, rtn_warning, debug_info = pipe(input_data, mode=mode, **self.params)
+        if rtn_code < 0:
+            raise Exception(f"Error in AnyText pipeline: {rtn_warning}")
+
+        return results
+
+# Node class and display name mappings
+NODE_CLASS_MAPPINGS = {
+    "AnyTextNode_Zho": AnyTextNode_Zho
+}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "AnyTextNode_Zho": "✒️AnyText"
+}
