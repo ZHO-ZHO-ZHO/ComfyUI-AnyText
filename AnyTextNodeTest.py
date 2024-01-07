bebd3243ec29dfe4c4e302a50c16a724be689406
ZHO-ZHO-ZHO <140084057+ZHO-ZHO-ZHO@users.noreply.github.com>
2024-01-07 15:54:22 +0800

    Update AnyTextNodeTest.py 测试

--- AnyTextNodeTest.py
+++ AnyTextNodeTest.py
@@ -1,4 +1,4 @@
-from .AnyText.cldm.model import create_model, load_state_dict
+#from .AnyText.cldm.model import create_model, load_state_dict
 from modelscope.pipelines import pipeline
 
 class AnyTextNode_Zho:
@@ -11,23 +11,20 @@ class AnyTextNode_Zho:
         return {
             "required": {
                 "prompt": ("STRING", {"default": "A cake with colorful characters that reads \"EVERYDAY\"", "multiline": True}),
-                "mode": (["text-generation", "text-editing"],),  
+                "mode": ("text-generation",),  
                 "image_count": ("INT", {"default": 2, "min": 1, "max": 10}),
                 "ddim_steps": ("INT", {"default": 20, "min": 1, "max": 100}),
                 "show_debug": ("BOOLEAN", {"default": True}),
                 "seed": ("INT", {"default": 0, "min": 0, "max": 99999999}),
                 "draw_pos": ("IMAGE",),
             },
-            "optional": {
-                "ori_image": ("IMAGE",),  
-            }
         }
 
     RETURN_TYPES = ("IMAGE",)
     CATEGORY = "✒️AnyText"
     FUNCTION = "run_anytext"
 
-    def run_anytext(self, mode, prompt, image_count, ddim_steps, show_debug, seed, draw_pos, ori_image):
+    def run_anytext(self, prompt, mode, image_count, ddim_steps, show_debug, seed, draw_pos):
         
         # Configure the pipeline for AnyText
         pipe = pipeline('my-anytext-task', model='damo/cv_anytext_text_generation_editing', model_revision='v1.1.1', use_fp16=True, use_translator=False)
@@ -43,11 +40,6 @@ class AnyTextNode_Zho:
             "seed": seed,
             "draw_pos": draw_pos
         }
-
-        if mode == 'text-generation':
-            pass
-        else:
-            input_data["ori_image"] = ori_image
 
         results, rtn_code, rtn_warning, debug_info = pipe(input_data, mode=mode, **self.params)
         if rtn_code < 0:
